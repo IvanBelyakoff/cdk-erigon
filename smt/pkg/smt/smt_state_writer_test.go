d123fc0174e089d6fbdfe4391bb995ae0e901043
Stefan Negovanović <stefan@ethernal.tech>
2024-11-06 11:44:59 +0100

    Restructure and add empty block test case

--- smt/pkg/smt/smt_state_writer_test.go
+++ smt/pkg/smt/smt_state_writer_test.go
@@ -15,30 +15,48 @@ import (
 )
 
 func TestSMTApplyTraces(t *testing.T) {
-	type testCase struct {
+	type testData struct {
 		Witness   string                                `json:"witness"`
 		StateRoot libcommon.Hash                        `json:"stateRoot"`
 		Traces    map[libcommon.Address]*types.TxnTrace `json:"traces,omitempty"`
 	}
 
-	data, err := os.ReadFile("./testdata/smt-traces.json")
-	require.NoError(t, err)
+	cases := []struct {
+		name string
+		file string
+	}{
+		{
+			name: "Empty block traces",
+			file: "./testdata/zerotraces/empty-block.json",
+		},
+		{
+			name: "Non-empty block traces",
+			file: "./testdata/zerotraces/non-empty-block.json",
+		},
+	}
 
-	var tc testCase
-	err = json.Unmarshal(data, &tc)
-	require.NoError(t, err)
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			rawTestData, err := os.ReadFile(c.file)
+			require.NoError(t, err)
 
-	decodedWitnessRaw, err := hex.DecodeString(strings.TrimPrefix(tc.Witness, "0x"))
-	require.NoError(t, err)
+			var td testData
+			err = json.Unmarshal(rawTestData, &td)
+			require.NoError(t, err)
 
-	witness, err := trie.NewWitnessFromReader(bytes.NewReader(decodedWitnessRaw), false)
-	require.NoError(t, err)
+			decodedWitnessRaw, err := hex.DecodeString(strings.TrimPrefix(td.Witness, "0x"))
+			require.NoError(t, err)
 
-	smt, err := BuildSMTfromWitness(witness)
-	require.NoError(t, err)
+			witness, err := trie.NewWitnessFromReader(bytes.NewReader(decodedWitnessRaw), false)
+			require.NoError(t, err)
 
-	newSMT, err := smt.ApplyTraces(tc.Traces)
-	require.NoError(t, err)
+			smt, err := BuildSMTfromWitness(witness)
+			require.NoError(t, err)
 
-	require.Equal(t, tc.StateRoot, libcommon.BigToHash(newSMT.LastRoot()))
+			newSMT, err := smt.ApplyTraces(td.Traces)
+			require.NoError(t, err)
+
+			require.Equal(t, td.StateRoot, libcommon.BigToHash(newSMT.LastRoot()))
+		})
+	}
 }
