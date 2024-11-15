78be1844f5b3c5cdca61a497bdce743470133684
Ji Hwan <jkim@polygon.technology>
2024-11-15 17:46:46 +0900

    fix: mod value not being padded + new test cases
    Signed-off-by: Ji Hwan <jkim@polygon.technology>

--- core/vm/contracts_zkevm.go
+++ core/vm/contracts_zkevm.go
@@ -397,22 +397,16 @@ func (c *bigModExp_zkevm) Run(input []byte) ([]byte, error) {
 		mod     = new(big.Int).SetBytes(getData(input, baseLen+expLen, modLen))
 	)
 
-	// Extract `base`, `exp`, and `mod` with padding as needed
-	baseData := getData(input, 96, uint64(baseLen))
-	if uint64(len(baseData)) < baseLen {
-		baseData = common.RightPadBytes(baseData, int(baseLen))
+	if len(input) >= 96+int(baseLen) {
+		base = new(big.Int).SetBytes(getData(input, 96, uint64(baseLen)))
 	}
-	base.SetBytes(baseData)
-
-	expData := getData(input, 96+uint64(baseLen), uint64(expLen))
-	if uint64(len(expData)) < expLen {
-		expData = common.RightPadBytes(expData, int(expLen))
+	if len(input) >= 96+int(baseLen)+int(expLen) {
+		exp = new(big.Int).SetBytes(getData(input, 96+uint64(baseLen), uint64(expLen)))
 	}
-	exp.SetBytes(expData)
-
-	modData := getData(input, 96+uint64(baseLen)+uint64(expLen), uint64(modLen))
-	if uint64(len(modData)) < modLen {
-		modData = common.RightPadBytes(modData, int(modLen))
+	// Always pad mod value to size of modLen. The rest will be ignored.
+	mod = new(big.Int).SetBytes(getData(input, 96+uint64(baseLen)+uint64(expLen), uint64(modLen)))
+	if len(input) < 96+int(baseLen)+int(expLen)+int(modLen) {
+		input = common.LeftPadBytes(input, 96+int(baseLen)+int(expLen)+int(modLen))
 	}
 	mod.SetBytes(modData)
 
