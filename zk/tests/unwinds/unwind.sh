c79c26f0ea6a8a55e8c171a076ab29ad377f37c6
Arthur Abeilice <afa7789@gmail.com>
2024-11-20 18:47:11 +0700

    chore(unwind-tests): changing script to run locally

--- zk/tests/unwinds/unwind.sh
+++ zk/tests/unwinds/unwind.sh
@@ -45,7 +45,7 @@ sleep 10
 # run erigon for a while to sync to the unwind point to capture the dump
 timeout $firstTimeout ./build/bin/cdk-erigon \
     --datadir="$dataPath/rpc-datadir" \
-    --config=./dynamic-integration8.yaml \
+    --config="zk/tests/unwinds/config/dynamic-integration8.yaml" \
     --zkevm.sync-limit=${firstStop}
 
 # now get a dump of the datadir at this point
@@ -54,7 +54,7 @@ go run ./cmd/hack --action=dumpAll --chaindata="$dataPath/rpc-datadir/chaindata"
 # now run to the final stop block
 timeout $secondTimeout ./build/bin/cdk-erigon \
     --datadir="$dataPath/rpc-datadir" \
-    --config=./dynamic-integration8.yaml \
+    --config="zk/tests/unwinds/config/dynamic-integration8.yaml" \
     --zkevm.sync-limit=${stopBlock}
 
 # now get a dump of the datadir at this point
@@ -63,7 +63,7 @@ go run ./cmd/hack --action=dumpAll --chaindata="$dataPath/rpc-datadir/chaindata"
 # now run the unwind
 go run ./cmd/integration state_stages_zkevm \
     --datadir="$dataPath/rpc-datadir" \
-    --config=./dynamic-integration8.yaml \
+    --config="zk/tests/unwinds/config/dynamic-integration8.yaml" \
     --chain=dynamic-integration \
     --unwind-batch-no=${unwindBatch}
 
@@ -95,7 +95,7 @@ done
 # now sync again
 timeout $secondTimeout ./build/bin/cdk-erigon \
     --datadir="$dataPath/rpc-datadir" \
-    --config=./dynamic-integration8.yaml \
+    --config="zk/tests/unwinds/config/dynamic-integration8.yaml" \
     --zkevm.sync-limit=${stopBlock}
 
 # dump the data again into the post folder
