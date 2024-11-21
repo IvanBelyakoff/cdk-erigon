3714e7fa14d851135b5bb9fa976ee142820cdc34
Arthur Abeilice <afa7789@gmail.com>
2024-11-21 14:34:18 +0700

    chore(unwind-test): changing timeout times to try and make it run without failure

--- zk/tests/unwinds/unwind.sh
+++ zk/tests/unwinds/unwind.sh
@@ -21,8 +21,8 @@ datastreamZipFileName="./datastream-net8-upto-11318-101.zip"
 firstStop=11204
 stopBlock=11315
 unwindBatch=70
-firstTimeout=300s
-secondTimeout=300s
+firstTimeout=450s
+secondTimeout=45s
 
 pushd $datastreamPath
     tar -xzf $datastreamZipFileName
@@ -31,50 +31,63 @@ popd
 rm -rf "$dataPath/rpc-datadir"
 rm -rf "$dataPath/phase1-dump1"
 rm -rf "$dataPath/phase1-dump2"
+
 rm -rf "$dataPath/phase2-dump1"
 rm -rf "$dataPath/phase2-dump2"
-rm -rf "$dataPath/phase1-diffs"
-rm -rf "$dataPath/phase2-diffs"  
+
+# rm -rf "$dataPath/phase1-diffs"
+# rm -rf "$dataPath/phase2-diffs"  
+
+pid=$(lsof -i :6900 | awk 'NR==2 {print $2}')
+kill -9 $pid
 
 # run datastream server
+echo -e '\nStarting datastream server \n'
 go run ./zk/debug_tools/datastream-host --file="$(pwd)/zk/tests/unwinds/datastream/hermez-dynamic-integration8-datastream/data-stream.bin" &
 
 # in order to start the datastream server
 sleep 10
 
+echo -e '\nStarting erigon \n'
 # run erigon for a while to sync to the unwind point to capture the dump
 timeout $firstTimeout ./build/bin/cdk-erigon \
     --datadir="$dataPath/rpc-datadir" \
     --config="zk/tests/unwinds/config/dynamic-integration8.yaml" \
     --zkevm.sync-limit=${firstStop}
 
+echo -e '\nDumping data \n'
+
 # now get a dump of the datadir at this point
 go run ./cmd/hack --action=dumpAll --chaindata="$dataPath/rpc-datadir/chaindata" --output="$dataPath/phase1-dump1"
 
+echo -e '\nRunning erigon one more time \n'
 # now run to the final stop block
 timeout $secondTimeout ./build/bin/cdk-erigon \
     --datadir="$dataPath/rpc-datadir" \
     --config="zk/tests/unwinds/config/dynamic-integration8.yaml" \
     --zkevm.sync-limit=${stopBlock}
 
+echo -e '\nDumping data phase 2 \n'
 # now get a dump of the datadir at this point
 go run ./cmd/hack --action=dumpAll --chaindata="$dataPath/rpc-datadir/chaindata" --output="$dataPath/phase2-dump1"
 
 # now run the unwind
+echo -e '\nUnwinding batch \n'
 go run ./cmd/integration state_stages_zkevm \
     --datadir="$dataPath/rpc-datadir" \
     --config="zk/tests/unwinds/config/dynamic-integration8.yaml" \
     --chain=dynamic-integration \
     --unwind-batch-no=${unwindBatch}
 
+echo -e '\nDumping data after unwind \n'
 # now get a dump of the datadir at this point
 go run ./cmd/hack --action=dumpAll --chaindata="$dataPath/rpc-datadir/chaindata" --output="$dataPath/phase1-dump2"
 
-
-mkdir -p "$dataPath/phase1-diffs/pre"
-mkdir -p "$dataPath/phase1-diffs/post"
+# mkdir -p "$dataPath/phase1-diffs/pre"
+# mkdir -p "$dataPath/phase1-diffs/post"
 
 # iterate over the files in the pre-dump folder
+# we are going to check if unwind worked
 for file in $(ls $dataPath/phase1-dump1); do
     # get the filename
     filename=$(basename $file)
@@ -83,26 +96,32 @@ for file in $(ls $dataPath/phase1-dump1); do
     if cmp -s $dataPath/phase1-dump1/$filename $dataPath/phase1-dump2/$filename; then
         echo "No difference found in $filename"
     else
+        # this is a list of files where we expect differences.
         if [ "$filename" = "Code.txt" ] || [ "$filename" = "HashedCodeHash.txt" ] || [ "$filename" = "hermez_l1Sequences.txt" ] || [ "$filename" = "hermez_l1Verifications.txt" ] || [ "$filename" = "HermezSmt.txt" ] || [ "$filename" = "PlainCodeHash.txt" ] || [ "$filename" = "SyncStage.txt" ] || [ "$filename" = "BadHeaderNumber.txt" ]; then
-            echo "Phase 1 Expected differences in $filename"
+            echo "Phase 1 - Expected differences in $filename"
         else
-            echo "Phase 1 Unexpected differences in $filename"
+            # unwind tests failed
+            echo "Phase 1 - Error unexpected differences in $filename"
+            echo "Unwind failed"
             exit 1
         fi
     fi
 done
 
 # now sync again
+# the data must match, if it doesn't match something is wrong, because if we unwinded returning to it should be the same.
+echo -e '\nRunning erigon to the same stopBlock again \n'
 timeout $secondTimeout ./build/bin/cdk-erigon \
     --datadir="$dataPath/rpc-datadir" \
     --config="zk/tests/unwinds/config/dynamic-integration8.yaml" \
     --zkevm.sync-limit=${stopBlock}
 
+echo -e '\nDumping data after unwind \n'
 # dump the data again into the post folder
 go run ./cmd/hack --action=dumpAll --chaindata="$dataPath/rpc-datadir/chaindata" --output="$dataPath/phase2-dump2"
 
-mkdir -p "$dataPath/phase2-diffs/pre"
-mkdir -p "$dataPath/phase2-diffs/post"
+# mkdir -p "$dataPath/phase2-diffs/pre"
+# mkdir -p "$dataPath/phase2-diffs/post"
 
 # iterate over the files in the pre-dump folder
 for file in $(ls $dataPath/phase2-dump1); do
@@ -113,10 +132,11 @@ for file in $(ls $dataPath/phase2-dump1); do
     if cmp -s $dataPath/phase2-dump1/$filename $dataPath/phase2-dump2/$filename; then
         echo "Phase 2 No difference found in $filename"
     else
+        # file were it should be different
         if [ "$filename" = "BadHeaderNumber.txt" ]; then
-            echo "Phase 2 Expected differences in $filename"
+            echo "Phase 2 - Expected differences in $filename"
         else
-            echo "Phase 2 Unexpected differences in $filename"
+            echo "Phase 2 - Error unexpected differences in $filename"
             exit 2
         fi
     fi
